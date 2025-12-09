5146c0890afbe0ad558e1656b41a1c3a24ac7b8a
kahyou22 <kahyou222@gmail.com>
2025-12-09 17:59:59 +0900

    1209-a

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const defaultSwiperOpts = {
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+};
+const swiperLeftOpts = {
+    ...defaultSwiperOpts,
+    pagination: {
+        el: ".banner .left .swiper-pagination",
+        clickable: true,
+    },
+};
+const swiperRightOpts = {
+    ...defaultSwiperOpts,
+    pagination: {
+        el: ".banner .right .swiper-pagination",
+        clickable: true,
+    },
+};
+const bannerSwiper1 = new Swiper(".banner .left", swiperLeftOpts);
+const bannerSwiper2 = new Swiper(".banner .right", swiperRightOpts);
