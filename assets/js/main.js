05ae397f75786962a680bf781b7c5365bd5053d5
kahyou22 <kahyou222@gmail.com>
2025-12-11 12:04:16 +0900

    1211-a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,41 @@
-
-const defaultSwiperOpts = {
+{
+  const defaultSwiperOpts = {
     spaceBetween: 10,
     centeredSlides: true,
     loop: true,
     slidesPerView: 1,
     autoplay: {
-        delay: 2500,
-        disableOnInteraction: true,
+      delay: 2500,
+      disableOnInteraction: true,
     },
-};
-const swiperLeftOpts = {
+  };
+  const swiperLeftOpts = {
     ...defaultSwiperOpts,
     pagination: {
-        el: ".banner .left .swiper-pagination",
-        clickable: true,
+      el: ".banner .left .swiper-pagination",
+      clickable: true,
     },
-};
-const swiperRightOpts = {
+  };
+  const swiperRightOpts = {
     ...defaultSwiperOpts,
     pagination: {
-        el: ".banner .right .swiper-pagination",
-        clickable: true,
+      el: ".banner .right .swiper-pagination",
+      clickable: true,
     },
-};
-const bannerSwiper1 = new Swiper(".banner .left", swiperLeftOpts);
-const bannerSwiper2 = new Swiper(".banner .right", swiperRightOpts);
+  };
+  const bannerSwiper1 = new Swiper(".banner .left", swiperLeftOpts);
+  const bannerSwiper2 = new Swiper(".banner .right", swiperRightOpts);
+}
+
+{
+  const swiperOpts = {
+    spaceBetween: 18,
+    loop: true,
+    slidesPerView: 4,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+  };
+  const recommendsSwiper = new Swiper(".recommends .right", swiperOpts);
+}
