9c0ac0ffe54789d06754c2b5a0255ebd7b58dcda
kahyou22 <kahyou222@gmail.com>
2025-12-11 17:48:35 +0900

    1211-b

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,14 +28,41 @@
 }
 
 {
+  const container = document.querySelector(
+    ".recommends .right .swiper-wrapper"
+  );
+  const slides = [...container.children];
+  for (let s of slides) {
+    container.appendChild(s.cloneNode(true));
+  }
   const swiperOpts = {
     spaceBetween: 18,
     loop: true,
     slidesPerView: 4,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
   };
   const recommendsSwiper = new Swiper(".recommends .right", swiperOpts);
+}
+
+{
+  const container = document.querySelector(
+    ".gift .right .swiper .swiper-wrapper"
+  );
+  const slides = [...container.children];
+  for (let s of slides) {
+    container.appendChild(s.cloneNode(true));
+  }
+  const swiperOpts = {
+    spaceBetween: 18,
+    loop: true,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  };
+  const giftSwiper = new Swiper(".gift .right .swiper", swiperOpts);
 }
